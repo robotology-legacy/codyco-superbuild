5fb9085a79dc4f2b32c582c6e873159c8d93c126
Silvio Traversaro <pegua1@gmail.com>
2014-03-19 00:55:39 +0100

    Force console_bridge to produce shared libs

--- cmake/Buildconsole_bridge.cmake
+++ cmake/Buildconsole_bridge.cmake
@@ -6,4 +6,5 @@ ycm_ep_helper(console_bridge TYPE GIT
               STYLE GITHUB
               REPOSITORY ros/console_bridge.git
               TAG master
-              COMPONENT external)
+              COMPONENT external
+              CMAKE_CACHE_ARGS -DBUILD_SHARED_LIBS:BOOL=ON)
