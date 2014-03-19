e20691fc6127928b2b69d0a71189bab5d42ff06c
Silvio Traversaro <pegua1@gmail.com>
2014-03-19 01:10:23 +0100

    Use kdl_codyco version 0.0.3

--- cmake/Buildkdl_codyco.cmake
+++ cmake/Buildkdl_codyco.cmake
@@ -7,6 +7,6 @@ find_or_build_package(orocos_kdl QUIET NO_CMAKE_PACKAGE_REGISTRY)
 ycm_ep_helper(kdl_codyco TYPE GIT
                          STYLE GITHUB
                          REPOSITORY traversaro/kdl_codyco.git
-                         TAG master
+                         TAG rovereto
                          COMPONENT external
                          DEPENDS orocos_kdl)
