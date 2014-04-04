34c6394e94efe6b49c9787450bec4046500afc8a
Silvio Traversaro <pegua1@gmail.com>
2014-04-04 12:38:31 +0200

    Create BuildparamHelp.cmake

---
+++ cmake/BuildparamHelp.cmake
@@ -0,0 +1,12 @@
+# CoDyCo
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(YARP QUIET)
+
+ycm_ep_helper(paramHelp TYPE GIT
+              STYLE GITHUB
+              REPOSITORY robotology-playground/paramHelp.git
+              TAG master
+              COMPONENT external
+              DEPENDS YARP)
