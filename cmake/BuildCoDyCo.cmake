4a50d7f0a9915ea2603386c8fb73c481e91049fd
iCub <icub@icub15.icub.local>
2014-05-08 11:41:48 +0200

    Corrected BuildCoDyCo.cmake

--- cmake/BuildCoDyCo.cmake
+++ cmake/BuildCoDyCo.cmake
@@ -14,10 +14,7 @@ find_or_build_package(ICUB QUIET)
 find_or_build_package(orocos_kdl QUIET NO_CMAKE_PACKAGE_REGISTRY)
 find_or_build_package(kdl_codyco QUIET NO_CMAKE_PACKAGE_REGISTRY)
 find_or_build_package(kdl_format_io QUIET NO_CMAKE_PACKAGE_REGISTRY)
-<<<<<<< HEAD
-#find_or_build_package(paramHelp)
-=======
->>>>>>> 1ee593afa8cf23a763df3a228b3f7d313708f6f1
+
 
 ycm_ep_helper(CoDyCo TYPE GIT
               STYLE GITHUB
