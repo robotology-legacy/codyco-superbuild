a529872b64f297ec18628897cb0309d64558465c
Silvio Traversaro <pegua1@gmail.com>
2014-07-17 22:26:57 +0200

    Remove cmake installation on os x
    Now cmake is installed by default

--- .ci/travis-deps.sh
+++ .ci/travis-deps.sh
@@ -37,6 +37,6 @@ elif [ "$TRAVIS_OS_NAME" = osx ]; then
     hdiutil unmount /Volumes/XQuartz-${XQUARTZ_VERSION}
     brew update
     brew tap homebrew/versions
-    brew install cmake eigen boost ace pkg-config gtk+ jpeg gtkmm sqlite readline gsl libglademm tinyxml
+    brew install eigen boost ace pkg-config gtk+ jpeg gtkmm sqlite readline gsl libglademm tinyxml
     brew install yarp --HEAD
 fi
