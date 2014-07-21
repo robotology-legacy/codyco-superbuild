a8430458c0be6b4501b7ad25e0eae96bcd283337
Silvio Traversaro <silvio.traversaro@iit.it>
2014-07-21 22:29:24 +0200

    refresh travis setup

--- .ci/travis-build.sh
+++ .ci/travis-build.sh
@@ -2,24 +2,12 @@
 
 set -e
 
-#if OS is linux or is not set
-if [ "$TRAVIS_OS_NAME" = linux -o -z "$TRAVIS_OS_NAME" ]; then
-    mkdir build 
-    cd build
-    if [ $CODYCO_ICUBWBI_USE_EXTERNAL_TORQUE ]; then
-        cmake -DNON_INTERACTIVE_BUILD:BOOL=TRUE -DCODYCO_ICUBWBI_USE_EXTERNAL_TORQUE_CONTROL:BOOL=TRUE ..
-    else 
-        cmake -DNON_INTERACTIVE_BUILD:BOOL=TRUE ..
-    fi
-    make -k
-elif [ "$TRAVIS_OS_NAME" = osx ]; then
-    mkdir build
-    cd build
-    cmake ..
-    make 
-    cd ..
-    mkdir build_xcode
-    cd build_xcode
-    cmake .. -GXcode
-    xcodebuild
+mkdir build
+cd build
+if [ $CODYCO_ICUBWBI_USE_EXTERNAL_TORQUE ]; then
+    cmake -G"${TRAVIS_CMAKE_GENERATOR}" -DCMAKE_BUILD_TYPE=${TRAVIS_BUILD_TYPE} -DNON_INTERACTIVE_BUILD:BOOL=TRUE -DCODYCO_ICUBWBI_USE_EXTERNAL_TORQUE_CONTROL:BOOL=TRUE ..
+else
+    cmake -G"${TRAVIS_CMAKE_GENERATOR}" -DCMAKE_BUILD_TYPE=${TRAVIS_BUILD_TYPE} -DNON_INTERACTIVE_BUILD:BOOL=TRUE ..
 fi
+cmake --build . --config ${TRAVIS_BUILD_TYPE}
+cmake --build . --config ${TRAVIS_BUILD_TYPE} --target install
