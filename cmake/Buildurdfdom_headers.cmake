3bc47967f2cc35ce1b6da27e19b91a95b7ef9e11
Silvio Traversaro <pegua1@gmail.com>
2014-10-19 18:44:23 +0200

    [Buildurdfdom_headers] remove console_bridge dependency

--- cmake/Buildurdfdom_headers.cmake
+++ cmake/Buildurdfdom_headers.cmake
@@ -3,12 +3,10 @@ include(YCMEPHelper)
 include(FindOrBuildPackage)
 
 find_or_build_package(TinyXML QUIET)
-find_or_build_package(console_bridge QUIET)
 
 ycm_ep_helper(urdfdom_headers TYPE GIT
               STYLE GITHUB
               REPOSITORY traversaro/urdfdom_headers.git
               TAG master
               COMPONENT external
-              DEPENDS console_bridge
-                      TinyXML)
+              DEPENDS TinyXML)
