4bb0c85b18142953f4a492f786f82dd797e1aeb7
Silvio Traversaro <silvio.traversaro@iit.it>
2014-10-29 11:31:01 +0100

    added new specific tags for repos

--- cmake/ProjectsTags.cmake
+++ cmake/ProjectsTags.cmake
@@ -1,3 +1,5 @@
 set(wholeBodyInterface_TAG new_wbi_ID)
 set(yarpWholeBodyInterface_TAG new_wbi_ID)
 set(codyco-modules_TAG new_wbi_ID)
+set(codycoCommons_TAG new_wbi_ID)
+set(iDynTree_TAG new_wbi_ID)
