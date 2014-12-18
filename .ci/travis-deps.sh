b0be66d3b9bbbce196a5c18840cac5d4826623b1
Silvio Traversaro <silvio.traversaro@iit.it>
2014-12-18 12:33:38 +0100

    travis configuration cleanup

--- .ci/travis-deps.sh
+++ .ci/travis-deps.sh
@@ -7,17 +7,17 @@ if [ "$TRAVIS_OS_NAME" = linux -o -z "$TRAVIS_OS_NAME" ]; then
     sudo sh -c 'echo "deb http://www.icub.org/ubuntu precise contrib/science" > /etc/apt/sources.list.d/icub.list'
     sudo add-apt-repository -y ppa:robotology/ppa
     if [ "$CODYCO_YARP_FROM_SUPERBUILD" = True ]; then
-        sudo add-apt-repository -y ppa:kubuntu-ppa/backports 
+        sudo add-apt-repository -y ppa:kubuntu-ppa/backports
     fi
     sudo apt-get update
     sudo apt-get install -qq libboost-system-dev libboost-thread-dev libtinyxml-dev
-    if [ "$CODYCO_YARP_FROM_SUPERBUILD" = True ]; then
+    #if [ "$CODYCO_YARP_FROM_SUPERBUILD" = True ]; then
         echo -e "codyco-superbuild travis testing: installing yarp from superbuild\n"
         sudo apt-get --force-yes install icub-common
-    else
-        echo -e "codyco-superbuild travis testing: installing yarp from deb packages\n"
-        sudo apt-get --force-yes install icub
-    fi
+    #else
+        #echo -e "codyco-superbuild travis testing: installing yarp from deb packages\n"
+        #sudo apt-get --force-yes install icub
+    #fi
     sudo apt-get install cmake
     #eigen 3.2 support
     hg clone https://bitbucket.org/eigen/eigen/
