89acc76473ab72141b52b9a4cf3b9b245b0052a4
Silvio Traversaro <pegua1@gmail.com>
2015-01-19 12:12:00 +0100

    Add ProjectTags.cmake for v0.1

---
+++ cmake/ProjectsTags.cmake
@@ -0,0 +1,8 @@
+set(wholeBodyInterface_TAG v0.1)
+set(yarpWholeBodyInterface_TAG v0.1)
+set(codyco-modules_TAG v0.1)
+set(codycoCommons_TAG v0.1)
+set(iDynTree_TAG v0.1)
+set(motorFrictionIdentificationLib_TAG v0.1)
+set(WBIToolbox_TAG v0.1)
+set(mexWholeBodyModel_TAG v0.1)
