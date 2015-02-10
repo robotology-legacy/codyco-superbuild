04fab61d18328286d4b4a028e8c694fede5b828f
Silvio Traversaro <pegua1@gmail.com>
2015-02-10 18:41:40 +0100

    Add cartesian controller when compiling icub-main
    using the superbuild

--- cmake/BuildICUB.cmake
+++ cmake/BuildICUB.cmake
@@ -11,4 +11,8 @@ ycm_ep_helper(ICUB TYPE GIT
                    COMPONENT external
                    CMAKE_CACHE_ARGS -DYARP_USE_GTK2:BOOL=ON
                                     -DICUB_SHARED_LIBRARY:BOOL=OFF
-                                    -DICUB_USE_GLUT:BOOL=ON)
+                                    -DICUB_USE_GLUT:BOOL=ON
+                                    -DENABLE_icubmod_cartesiancontrollerclient:BOOL=ON
+                                    -DENABLE_icubmod_cartesiancontrollerclient:BOOL=ON
+                                    -DENABLE_icubmod_debug_InterfaceClient:BOOL=ON
+                                    -DENABLE_icubmod_gazecontrollerclient:BOOL=ON)
