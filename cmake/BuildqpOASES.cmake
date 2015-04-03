b2c79271d52b1305f74e3bd1faebc45e172854dd
Silvio Traversaro <silvio.traversaro@iit.it>
2015-04-03 10:23:22 +0200

    Point BuildqpOASES to our local fork for the moment

--- cmake/BuildqpOASES.cmake
+++ cmake/BuildqpOASES.cmake
@@ -1,5 +1,5 @@
 #.rst:
-# BuildECM
+# BuildqpOASES
 # --------
 #
 # qpOASES
@@ -18,18 +18,11 @@
 # (To distribute this file outside of YCM, substitute the full
 #  License text for the above reference.)
 
-# TYPE SVN STYLE COIN_OR
-set(YCM_SVN_COIN_OR_USERNAME "" CACHE STRING "Username to use for coin-or svn repositories")
-set(YCM_SVN_COIN_OR_PASSWORD "" CACHE STRING "Password to use for coin-or svn repositories")
-set(YCM_SVN_COIN_OR_BASE_ADDRESS "https://projects.coin-or.org/svn/" CACHE INTERNAL "Address to use for coin-or svn repositories")
-mark_as_advanced(YCM_SVN_COIN_OR_USERNAME YCM_SVN_COIN_OR_PASSWORD)
-
 # qpOASES
 include(YCMEPHelper)
 
-ycm_ep_helper(qpOASES TYPE SVN
-                      STYLE COIN_OR
-                      REPOSITORY qpOASES/stable/3.1
-                      TRUST_CERT 1
-                      COMPONENT external
-                      INSTALL_COMMAND "")
+ycm_ep_helper(qpOASES TYPE GIT
+              STYLE GITHUB
+              REPOSITORY robotology-playground/qpOASES.git
+              TAG master
+              COMPONENT external)
