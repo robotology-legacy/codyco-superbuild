7a4420662a3352026ba703dbe87a79c0e191cfe3
Silvio Traversaro <pegua1@gmail.com>
2015-06-24 20:41:44 +0200

    Import BuildiDynTree.cmake from iDynTree-superbuild
    To drop boost dependency on Windows

--- cmake/BuildiDynTree.cmake
+++ cmake/BuildiDynTree.cmake
@@ -1,4 +1,4 @@
-# CoDyCo
+# iDynTree
 include(YCMEPHelper)
 include(FindOrBuildPackage)
 
@@ -11,9 +11,26 @@ find_or_build_package(ICUB QUIET)
 # directory, and therefore will not rebuild the package.
 # Therefore we disable the package registry using
 # NO_CMAKE_PACKAGE_REGISTRY
+find_or_build_package(TinyXML QUIET)
 find_or_build_package(orocos_kdl QUIET NO_CMAKE_PACKAGE_REGISTRY)
+if( NOT MSVC )
 find_or_build_package(urdfdom_headers QUIET)
 find_or_build_package(urdfdom QUIET)
+endif()
+
+if( MSVC )
+ycm_ep_helper(iDynTree TYPE GIT
+              STYLE GITHUB
+              REPOSITORY robotology-playground/iDynTree.git
+              TAG master
+              COMPONENT libraries
+              CMAKE_CACHE_ARGS -DIDYNTREE_ENABLE_URDF:BOOL=ON
+              CMAKE_ARGS -DIDYNTREE_USES_MATLAB:BOOL=${IDYNTREE_USES_MATLAB}
+              DEPENDS YARP
+                      ICUB
+                      orocos_kdl
+					  TinyXML)
+else()
 
 ycm_ep_helper(iDynTree TYPE GIT
               STYLE GITHUB
@@ -26,4 +43,6 @@ ycm_ep_helper(iDynTree TYPE GIT
                       ICUB
                       orocos_kdl
                       urdfdom_headers
-                      urdfdom)
+                      urdfdom
+					  TinyXML)
+endif()
