49d54b0e67d02955ad8a719863d6a2a03f034624
Silvio Traversaro <pegua1@gmail.com>
2016-01-25 10:17:30 +0100

    Update travis-build.sh

--- .ci/travis-build.sh
+++ .ci/travis-build.sh
@@ -9,4 +9,9 @@ if [ "${TRAVIS_CMAKE_GENERATOR}" = Xcode ]; then
     xcbuild -configuration ${TRAVIS_BUILD_TYPE}
 else
     cmake --build . --config ${TRAVIS_BUILD_TYPE}
+    # build two times: one for normal use and one to make sure that some project 
+    # is not incorrectly using the installed headers instead of the build one 
+    # see https://github.com/robotology/idyntree/issues/52 for more details
+    (cd ./install/include && find . -type f -exec /bin/sh -c "> '{}'" ';')
+    cmake --build . --config ${TRAVIS_BUILD_TYPE}
 fi
