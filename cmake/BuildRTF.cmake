c5d0ea6993cdb69310a3a21b836a6f739c00b340
Silvio Traversaro <pegua1@gmail.com>
2016-03-22 13:45:15 +0000

    [cmake] pass options to RTF

--- cmake/BuildRTF.cmake
+++ cmake/BuildRTF.cmake
@@ -5,4 +5,7 @@ include(FindOrBuildPackage)
 ycm_ep_helper(RTF TYPE GIT
                   STYLE GITHUB
                   REPOSITORY robotology/robot-testing.git
-                  COMPONENT external)
+                  COMPONENT external
+                  CMAKE_CACHE_ARGS -DENABLE_MIDDLEWARE_PLUGINS:BOOL=ON
+                                   -DENABLE_LUA_PLUGIN:BOOL=${CODYCO_USES_LUA}
+                                   -DENABLE_PYTHON_PLUGIN:BOOL=${CODYCO_USES_PYTHON})
