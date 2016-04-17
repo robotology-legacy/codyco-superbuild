fcc182380e44a1a4d1d45c05209c4a34fa936a26
Silvio Traversaro <pegua1@gmail.com>
2016-04-17 22:16:42 +0200

    Add useful apt packages to the installed dependencies

--- .ci/travis-deps.sh
+++ .ci/travis-deps.sh
@@ -6,7 +6,7 @@ set -e
 if [ "$TRAVIS_OS_NAME" = linux -o -z "$TRAVIS_OS_NAME" ]; then
     sudo sh -c 'echo "deb http://www.icub.org/ubuntu trusty contrib/science" > /etc/apt/sources.list.d/icub.list'
     sudo apt-get update
-    sudo apt-get -y --force-yes install -qq libboost-filesystem-dev libboost-system-dev libboost-thread-dev libtinyxml-dev libboost-iostreams-dev icub-common cmake libeigen3-dev
+    sudo apt-get -y --force-yes install -qq libboost-filesystem-dev libboost-system-dev libboost-thread-dev libtinyxml-dev libboost-iostreams-dev icub-common  valgrind liblua5.1-0-dev lua5.1 swig python-dev cmake libeigen3-dev
 elif [ "$TRAVIS_OS_NAME" = osx ]; then
     gem install xcpretty
     brew update &> /dev/null
