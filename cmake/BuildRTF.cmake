d45038ce9455cc36094a953687e5b3449b4f6894
Silvio Traversaro <silvio.traversaro@iit.it>
2016-04-19 20:58:40 +0200

    Fix BuildRTF

--- cmake/BuildRTF.cmake
+++ cmake/BuildRTF.cmake
@@ -9,7 +9,7 @@ ycm_ep_helper(RTF TYPE GIT
                   REPOSITORY robotology/robot-testing.git
                   CMAKE_CACHE_ARGS -DENABLE_MIDDLEWARE_PLUGINS:BOOL=ON
                                    -DENABLE_LUA_PLUGIN:BOOL=${CODYCO_USES_LUA}
-                                   -DENABLE_PYTHON_PLUGIN:BOOL=${CODYCO_USES_PYTHON})
+                                   -DENABLE_PYTHON_PLUGIN:BOOL=${CODYCO_USES_PYTHON}
                   DEPENDS YARP
                   COMPONENT external)
 
