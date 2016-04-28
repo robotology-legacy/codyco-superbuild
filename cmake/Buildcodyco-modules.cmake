7f6f1b118b4d393d7cae51bdf26774555ceacb79
iCubGenova02 <icubgenova02@icub.iit.it>
2016-04-28 17:14:18 +0200

    [cmake] Added CODYCO_USES_KDL option, to compile the superbuild without using KDL

--- cmake/Buildcodyco-modules.cmake
+++ cmake/Buildcodyco-modules.cmake
@@ -7,40 +7,24 @@ find_or_build_package(ICUB QUIET)
 
 find_or_build_package(iDynTree QUIET)
 find_or_build_package(paramHelp QUIET)
-find_or_build_package(wholeBodyInterface QUIET NO_CMAKE_PACKAGE_REGISTRY)
-find_or_build_package(yarpWholeBodyInterface QUIET)
 
-find_or_build_package(codycoCommons QUIET)
+set(codyco-module_DEPENDS)
+
+if(${CODYCO_USES_KDL})
+    find_or_build_package(wholeBodyInterface QUIET NO_CMAKE_PACKAGE_REGISTRY)
+    find_or_build_package(yarpWholeBodyInterface QUIET)
+    find_or_build_package(codycoCommons QUIET)
+    
+    list(APPEND codyco-module_DEPENDS wholeBodyInterface)
+    list(APPEND codyco-module_DEPENDS yarpWholeBodyInterface)
+    list(APPEND codyco-module_DEPENDS codycoCommons)
+endif()
 
 if (${CODYCO_USES_OROCOS_BFL_BERDY})
-	find_or_build_package(orocosBFLBerdy QUIET)
+    find_or_build_package(orocosBFLBerdy QUIET)
 endif()
 
-set(CODYCO_CMAKE_CACHE_ARGS_USER_DEFINED "")
-if(${CODYCO_USES_URDFDOM})
-    set(CODYCO_CMAKE_CACHE_ARGS_USER_DEFINED ${CODYCO_CMAKE_CACHE_ARGS_USER_DEFINED} -DCODYCO_USES_URDFDOM:BOOL=ON)
-elseif()
-    set(CODYCO_CMAKE_CACHE_ARGS_USER_DEFINED ${CODYCO_CMAKE_CACHE_ARGS_USER_DEFINED} -DCODYCO_USES_URDFDOM:BOOL=OFF)
-endif()
 
-if(${CODYCO_USES_WBI_TOOLBOX})
-    set(CODYCO_CMAKE_CACHE_ARGS_USER_DEFINED ${CODYCO_CMAKE_CACHE_ARGS_USER_DEFINED} -DCODYCO_USES_WBI_TOOLBOX:BOOL=ON)
-elseif()
-    set(CODYCO_CMAKE_CACHE_ARGS_USER_DEFINED ${CODYCO_CMAKE_CACHE_ARGS_USER_DEFINED} -DCODYCO_USES_WBI_TOOLBOX:BOOL=OFF)
-endif()
-
-if(${CODYCO_TRAVIS_CI})
-    set(CODYCO_CMAKE_CACHE_ARGS_USER_DEFINED ${CODYCO_CMAKE_CACHE_ARGS_USER_DEFINED} -DCODYCO_TRAVIS_CI:BOOL=ON)
-elseif()
-    set(CODYCO_CMAKE_CACHE_ARGS_USER_DEFINED ${CODYCO_CMAKE_CACHE_ARGS_USER_DEFINED} -DCODYCO_TRAVIS_CI:BOOL=OFF)
-endif()
-
-if(${CODYCO_USES_OROCOS_BFL_BERDY})
-    set(CODYCO_CMAKE_CACHE_ARGS_USER_DEFINED ${CODYCO_CMAKE_CACHE_ARGS_USER_DEFINED} -DCODYCO_USES_OROCOS_BFL_BERDY:BOOL=ON)
-    set(CODYCO_OROCOS_BFL_BERDY_DEPENDENCY orocosBFLBerdy)
-elseif()
-    set(CODYCO_CMAKE_CACHE_ARGS_USER_DEFINED ${CODYCO_CMAKE_CACHE_ARGS_USER_DEFINED} -DCODYCO_USES_OROCOS_BFL_BERDY:BOOL=OFF)
-endif()
 
 ycm_ep_helper(codyco-modules TYPE GIT
               STYLE GITHUB
@@ -48,6 +32,9 @@ ycm_ep_helper(codyco-modules TYPE GIT
               TAG master
               COMPONENT main
               CMAKE_CACHE_ARGS ${CODYCO_CMAKE_CACHE_ARGS_USER_DEFINED}
+              CMAKE_ARGS -DCODYCO_TRAVIS_CI:BOOL=${CODYCO_TRAVIS_CI}
+                         -DCODYCO_USES_OROCOS_BFL_BERDY:BOOL=${CODYCO_USES_OROCOS_BFL_BERDY}
+                         -DCODYCO_USES_KDL:BOOL=${CODYCO_USES_KDL}
               DEPENDS YARP
                       ICUB
                       iDynTree
--- cmake/BuildiDynTree.cmake
+++ cmake/BuildiDynTree.cmake
@@ -4,45 +4,42 @@ include(FindOrBuildPackage)
 
 find_or_build_package(YARP QUIET)
 find_or_build_package(ICUB QUIET)
-# orocos_kdl and package kdl_codyco use export(PACKAGE <pkg>) that
-# installs some files in ~/.cmake/packages/<pkg> that are used by cmake
-# to locate the build directory for some packages.
-# This messes up with the superbuild, because CMake will find the build
-# directory, and therefore will not rebuild the package.
-# Therefore we disable the package registry using
-# NO_CMAKE_PACKAGE_REGISTRY
+
 find_or_build_package(TinyXML QUIET)
-find_or_build_package(orocos_kdl QUIET NO_CMAKE_PACKAGE_REGISTRY)
-if( NOT MSVC )
-find_or_build_package(urdfdom_headers QUIET)
-find_or_build_package(urdfdom QUIET)
+
+# iDynTree is being migrated to be indipendent from urdfdom/orocos_kdl and thus boost
+# Consequently, depending on the CODYCO_USES_KDL option it compile the part that depends 
+# on Boost (kdl, model_io, etc) or not
+set(iDynTree_DEPENDS)
+
+if(${CODYCO_USES_KDL})
+    # orocos_kdl and package kdl_codyco use export(PACKAGE <pkg>) that
+    # installs some files in ~/.cmake/packages/<pkg> that are used by cmake
+    # to locate the build directory for some packages.
+    # This messes up with the superbuild, because CMake will find the build
+    # directory, and therefore will not rebuild the package.
+    # Therefore we disable the package registry using
+    # NO_CMAKE_PACKAGE_REGISTRY
+    find_or_build_package(orocos_kdl QUIET NO_CMAKE_PACKAGE_REGISTRY)
+    list(APPEND iDynTree_DEPENDS orocos_kdl)
+
+    if( NOT MSVC )
+        find_or_build_package(urdfdom_headers QUIET)
+        find_or_build_package(urdfdom QUIET)
+        list(APPEND iDynTree_DEPENDS urdfdom_headers)
+        list(APPEND iDynTree_DEPENDS urdfdom)
+    endif()
 endif()
 
-if( MSVC )
+
 ycm_ep_helper(iDynTree TYPE GIT
               STYLE GITHUB
               REPOSITORY robotology/idyntree.git
               TAG master
               COMPONENT libraries
-              CMAKE_CACHE_ARGS -DIDYNTREE_ENABLE_URDF:BOOL=ON
               CMAKE_ARGS -DIDYNTREE_USES_MATLAB:BOOL=${CODYCO_USES_MATLAB}
+                         -DIDYNTREE_USES_KDL=${CODYCO_USES_KDL}
               DEPENDS YARP
                       ICUB
-                      orocos_kdl
-                      TinyXML)
-else()
-
-ycm_ep_helper(iDynTree TYPE GIT
-              STYLE GITHUB
-              REPOSITORY robotology/idyntree.git
-              TAG master
-              COMPONENT libraries
-              CMAKE_CACHE_ARGS -DIDYNTREE_ENABLE_URDF:BOOL=ON
-              CMAKE_ARGS -DIDYNTREE_USES_MATLAB:BOOL=${CODYCO_USES_MATLAB}
-              DEPENDS YARP
-                      ICUB
-                      orocos_kdl
-                      urdfdom_headers
-                      urdfdom
-		      TinyXML)
-endif()
+                      TinyXML
+                      ${iDynTree_DEPENDS})
