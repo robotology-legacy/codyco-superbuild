65b9d5d2389c102d7f16fd65d5db3a401eebaff8
Silvio Traversaro <pegua1@gmail.com>
2016-05-24 14:33:28 +0200

    [cmake] On Windows use Eigen 3.3-beta1
    Instead of the default branch that tends to break

---
+++ cmake/BuildEigen3.cmake
@@ -0,0 +1,26 @@
+#.rst:
+# BuildEigen3
+# -----------
+#
+# Eigen3
+
+#=============================================================================
+# Copyright 2014 iCub Facility, Istituto Italiano di Tecnologia
+#   Authors: Daniele E. Domenichelli <daniele.domenichelli@iit.it>
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of YCM, substitute the full
+#  License text for the above reference.)
+
+include(YCMEPHelper)
+
+ycm_ep_helper(Eigen3 TYPE HG
+                     STYLE BITBUCKET
+                     REPOSITORY eigen/eigen
+                     TAG 3.3-beta1)
