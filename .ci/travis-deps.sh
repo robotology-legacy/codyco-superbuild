c623d11db3fafdec65325e92b580879415e5af6d
Silvio Traversaro <pegua1@gmail.com>
2017-03-15 09:57:35 +0100

    [travis, macOS ] Remove installation of lua and octave from homebrew

--- .ci/travis-deps.sh
+++ .ci/travis-deps.sh
@@ -15,5 +15,5 @@ elif [ "$TRAVIS_OS_NAME" = osx ]; then
     brew tap homebrew/science
     source .ci/brew_install_or_upgrade_formula.sh
     # brew tap homebrew/versions #useful only if we need a particular version.
-    brewInstallFormulas eigen boost ace pkg-config jpeg swig sqlite readline gsl tinyxml lua octave yarp
+    brewInstallFormulas eigen boost ace pkg-config jpeg swig sqlite readline gsl tinyxml yarp
 fi
