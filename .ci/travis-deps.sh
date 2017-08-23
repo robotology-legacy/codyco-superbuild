5dfdd62dd195c9865a070f797abeb3de2e2eab4b
Silvio Traversaro <pegua1@gmail.com>
2017-08-23 19:34:48 +0200

    [travis] Add PPA for Eigen 3.3

--- .ci/travis-deps.sh
+++ .ci/travis-deps.sh
@@ -5,6 +5,7 @@ set -e
 #if OS is linux or is not set
 if [ "$TRAVIS_OS_NAME" = linux -o -z "$TRAVIS_OS_NAME" ]; then
     sudo add-apt-repository ppa:octave/stable -y
+    sudo add-apt-repository ppa:nschloe/eigen-backports -y
     sudo sh -c 'echo "deb http://www.icub.org/ubuntu trusty contrib/science" > /etc/apt/sources.list.d/icub.list'
     sudo apt-get update
     sudo apt-get -y --force-yes install -qq libboost-filesystem-dev libboost-system-dev libboost-thread-dev libtinyxml-dev libboost-iostreams-dev icub-common  valgrind liblua5.1-0-dev lua5.1 swig python-dev cmake libeigen3-dev liboctave-dev
