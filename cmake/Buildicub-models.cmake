a7b4f012dfccbf8ad39ebd2aeca30bcf2835b377
Silvio Traversaro <pegua1@gmail.com>
2017-08-28 13:56:59 +0200

    Revert "Add icub-models repository to the superbuild"

--- cmake/Buildicub-models.cmake
+++
@@ -1,8 +0,0 @@
-include(YCMEPHelper)
-
-ycm_ep_helper(icub-models
-              TYPE GIT
-              STYLE GITHUB
-              REPOSITORY robotology-playground/icub-models.git
-              TAG master
-              COMPONENT main)
