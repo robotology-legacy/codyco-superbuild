68dcb0fa96ca7d25819a7a4d02c196b4adbe1ded
Gabriele <gabriele.nava@mail.polimi.it>
2017-09-06 11:13:20 +0200

    added icub-models to the superbuild

---
+++ cmake/Buildicub-models.cmake
@@ -0,0 +1,8 @@
+include(YCMEPHelper)
+
+ycm_ep_helper(icub-models
+              TYPE GIT
+              STYLE GITHUB
+              REPOSITORY robotology-playground/icub-models.git
+              TAG master
+              COMPONENT main)
